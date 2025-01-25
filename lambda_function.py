d2d56421ec8531b761160080c4d5e5bcb2f553eb
AfonsoCastro1983 <afonso.castro@gmail.com>
2025-01-25 15:42:18 -0400

    Versão final

--- lambda_function.py
+++ lambda_function.py
@@ -2,14 +2,28 @@ import boto3
 import jwt
 import json
 import uuid
+import os
+from datetime import datetime
+from urllib.parse import parse_qs
 
-s3_client = boto3.client('s3')
+s3_client = boto3.client('s3',aws_access_key_id=os.getenv('va_aws_key_id'),aws_secret_access_key=os.getenv('va_aws_key_secret'))
 dynamodb_client = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
     # Obter o token JWT do header Authorization
     token = event['headers']['Authorization'].split(" ")[1]
     body = event['body']
+    print('token', token)
+    print('body', body)
+
+    # Decodificar os dados do formato x-www-form-urlencoded
+    form_data = parse_qs(body)
+    # Obter o valor do campo 'nome_arquivo'
+    nome_arquivo = form_data.get('nome_arquivo', [None])[0]
+
+    # Gerando IDs
+    video_id = str(uuid.uuid4())
+    bucket_name = "amz.video-upload.bucket"
 
     # Decodificar o token JWT
     try:
@@ -25,12 +39,31 @@ def lambda_handler(event, context):
             "body": f"Token inválido: {str(e)}"
         }
 
-    resposta = {
-        id_user: user_id,
-        resposta: 'Upload Concluído'
+    presigned_url_s3 = s3_client.generate_presigned_url(
+        'put_object',
+        Params={'Bucket': bucket_name, 'Key': f'videos/{video_id}/{nome_arquivo}'},
+        ExpiresIn=3600
+    )
+
+    # Adicionando data e hora
+    timestamp = datetime.utcnow().isoformat()
+
+    retorno = {
+        "userId": user_id,
+        "videoId": video_id,
+        "videoName": nome_arquivo,
+        "timestamp": timestamp,
+        "s3_key": f'videos/{video_id}/{nome_arquivo}',
+        "presigned_url": presigned_url_s3
     }
+
+    # Salvando no DynamoDB
+    table = dynamodb_client.Table("VideosTable")
+    table.put_item(
+        Item=retorno
+    )
 
     return {
         "statusCode": 200,
-        "body": json.dumps(resposta)
+        "body": json.dumps(retorno)
     }
